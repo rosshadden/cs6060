df040cd971b887e82dea0761c14a2f1245975c07
Ross Hadden <rosshadden@gmail.com>
2013-08-05 08:38:28 -0400

    Added comments and reorganized some things.

--- 3/main.js
+++ 3/main.js
@@ -3,8 +3,6 @@
 
 	// GLOBALS
 	var scene, camera, renderer;
-
-	var PI = Math.PI;
 	var axes = {
 		x: new THREE.Vector3(1, 0, 0),
 		y: new THREE.Vector3(0, 1, 0),
@@ -44,18 +42,15 @@
 
 	// SETUP
 	(function() {
+		// Set up properties of the camera.
 		camera.speed = 1;
 		camera.lookAt(axes.z);
 		camera.position.set(0, 0, -48);
-		scene.add(camera);
-
-		// Show axes, for debugging.
-		// var axisHelper = new THREE.AxisHelper(10);
-		// scene.add(axisHelper);
 	})();
 
 	// MODELS
 	var models = (function() {
+		// Save the wallpaper material for use on each wall.
 		var wallMaterial = new THREE.MeshLambertMaterial({
 			side: THREE.DoubleSide,
 			map: (function() {
@@ -91,7 +86,7 @@
 					new THREE.PlaneGeometry(128, 64),
 					wallMaterial
 				)
-				.rotateY(PI / 2)
+				.rotateY(Math.PI / 2)
 				.translateX(0)
 				.translateY(0)
 				.translateZ(-64)
@@ -101,7 +96,7 @@
 					new THREE.PlaneGeometry(128, 64),
 					wallMaterial
 				)
-				.rotateY(-PI / 2)
+				.rotateY(-Math.PI / 2)
 				.translateX(0)
 				.translateY(0)
 				.translateZ(-64)
@@ -119,7 +114,7 @@
 						})()
 					})
 				)
-				.rotateX(PI / 2)
+				.rotateX(Math.PI / 2)
 				.translateX(0)
 				.translateY(0)
 				.translateZ(-32)
@@ -137,7 +132,7 @@
 						})()
 					})
 				)
-				.rotateX(PI / 2)
+				.rotateX(Math.PI / 2)
 				.translateX(0)
 				.translateY(0)
 				.translateZ(32)
@@ -145,11 +140,14 @@
 
 		// Create the chair model.
 		var chair = (function() {
+			// Make the overall chair object.
 			var chair = new THREE.Geometry();
+			// Set the chair material here for reuse.
 			var material = new THREE.MeshLambertMaterial({
 				map: new THREE.ImageUtils.loadTexture("../images/crate.png")
 			});
 
+			// Make blueprints for all the individual parts of the chair.
 			var chairParts = {
 				back: new THREE.Mesh(
 					new THREE.CubeGeometry(12, 16, 1), null
@@ -167,6 +165,8 @@
 					.translateY(-13)
 			};
 
+			// Create four legs,
+			// by cloning and transforming the above blueprint.
 			chairParts.legs = [
 				chairParts.leg.clone()
 					.translateX(-4)
@@ -182,36 +182,43 @@
 					.translateZ(10)
 			];
 
+			// Tell the renderer which materials to use for each face or part.
 			var materials = [
 				chairParts.seat.material,
 				material
 			];
-
 			THREE.GeometryUtils.setMaterialIndex(chairParts.seat.geometry, 0);
 			THREE.GeometryUtils.setMaterialIndex(chairParts.back.geometry, 1);
+
+			// Merge all of the individual chair parts into the overall chair object.
 			THREE.GeometryUtils.merge(chair, chairParts.seat);
 			THREE.GeometryUtils.merge(chair, chairParts.back);
-
 			chairParts.legs.forEach(function(leg) {
 				THREE.GeometryUtils.setMaterialIndex(leg.geometry, 1);
 				THREE.GeometryUtils.merge(chair, leg);
 			});
 
+			// Return the overall chair object,
+			// applying the materials defined above.
 			return new THREE.Mesh(chair, new THREE.MeshFaceMaterial(materials))
-				.rotateY(PI/2)
+				.rotateY(Math.PI/2)
 				.translateX(-48)
 				.translateY(-15)
 				.translateZ(-48);
 		})();
 
+		// Create the tube model that houses the floating face.
 		var tube = new THREE.Mesh(
 			new THREE.CylinderGeometry(16, 16, 62, 64),
 			new THREE.MeshLambertMaterial({ color: 0x7EB6FF, transparent: true, opacity: 0.4 })
 		);
 
+		// Create the creepy floating face.
 		var face = (function() {
+			// Make the overall face object.
 			var face = new THREE.Geometry();
 
+			// Make the blueprints for the individual parts to the face.
 			var head = new THREE.SphereGeometry(12, 64, 64);
 
 			var nose = new THREE
@@ -220,7 +227,7 @@
 					new THREE.MeshLambertMaterial({ color: 0xeebb99 })
 				)
 				.translateZ(-12.5)
-				.rotateX(PI / 2)
+				.rotateX(Math.PI / 2)
 			;
 
 			var eye = new THREE
@@ -230,9 +237,10 @@
 				)
 				.translateY(4)
 				.translateZ(-11)
-				.rotateX(PI / 2)
+				.rotateX(Math.PI / 2)
 			;
 
+			// Make the eyes by cloning and transforming the above eye blueprint.
 			var eyes = [
 				eye.clone().translateX(3),
 				eye.clone().translateX(-3)
@@ -245,9 +253,11 @@
 				)
 				.translateY(-4)
 				.translateZ(-11.3)
-				.rotateX(PI / 2.5)
-				.rotateY(PI)
+				.rotateX(Math.PI / 2.5)
+				.rotateY(Math.PI)
 			;
+			mouth.scale.x = 20;
+			mouth.scale.z = 16;
 
 			var tooth = new THREE
 				.Mesh(
@@ -259,29 +269,13 @@
 				.rotateX(0.5)
 			;
 
+			// Make the teeth by cloning and transforming the above tooth blueprint.
 			var teeth = [
 				tooth.clone().translateX(1.2),
 				tooth.clone().translateX(-1.2)
 			];
 
-			mouth.scale.x = 20;
-			mouth.scale.z = 16;
-
-			THREE.GeometryUtils.setMaterialIndex(head, 0);
-			THREE.GeometryUtils.setMaterialIndex(nose.geometry, 1);
-			THREE.GeometryUtils.setMaterialIndex(eyes[0].geometry, 2);
-			THREE.GeometryUtils.setMaterialIndex(eyes[1].geometry, 2);
-			THREE.GeometryUtils.setMaterialIndex(mouth.geometry, 3);
-			THREE.GeometryUtils.setMaterialIndex(teeth[0].geometry, 2);
-			THREE.GeometryUtils.setMaterialIndex(teeth[1].geometry, 2);
-			THREE.GeometryUtils.merge(face, head);
-			THREE.GeometryUtils.merge(face, nose);
-			THREE.GeometryUtils.merge(face, eyes[0]);
-			THREE.GeometryUtils.merge(face, eyes[1]);
-			THREE.GeometryUtils.merge(face, mouth);
-			THREE.GeometryUtils.merge(face, teeth[0]);
-			THREE.GeometryUtils.merge(face, teeth[1]);
-
+			// Setup the materials for each individual face part.
 			var materials = [
 				new THREE.MeshLambertMaterial({ color: 0xFFCC99 }),
 				nose.material,
@@ -290,6 +284,25 @@
 				tooth.material
 			];
 
+			// Tell the renderer which materials to use for each model in the overall face.
+			THREE.GeometryUtils.setMaterialIndex(head, 0);
+			THREE.GeometryUtils.setMaterialIndex(nose.geometry, 1);
+			THREE.GeometryUtils.setMaterialIndex(eyes[0].geometry, 2);
+			THREE.GeometryUtils.setMaterialIndex(eyes[1].geometry, 2);
+			THREE.GeometryUtils.setMaterialIndex(mouth.geometry, 3);
+			THREE.GeometryUtils.setMaterialIndex(teeth[0].geometry, 2);
+			THREE.GeometryUtils.setMaterialIndex(teeth[1].geometry, 2);
+
+			// Merge all of the individual face parts into the overall object.
+			THREE.GeometryUtils.merge(face, head);
+			THREE.GeometryUtils.merge(face, nose);
+			THREE.GeometryUtils.merge(face, eyes[0]);
+			THREE.GeometryUtils.merge(face, eyes[1]);
+			THREE.GeometryUtils.merge(face, mouth);
+			THREE.GeometryUtils.merge(face, teeth[0]);
+			THREE.GeometryUtils.merge(face, teeth[1]);
+
+			// Return the overall face object, with the above material map applied.
 			return new THREE.Mesh(face, new THREE.MeshFaceMaterial(materials));
 		})();
 
@@ -303,6 +316,9 @@
 
 	// DRAW
 	(function() {
+		// Add the main camera.
+		scene.add(camera);
+
 		// Add all of the walls.
 		scene.add(models.walls.north);
 		scene.add(models.walls.south);
@@ -361,19 +377,19 @@
 		}
 		// Look up.
 		if (KeyboardJS.combo.active("up")) {
-			camera.rotateX(+PI/64);
+			camera.rotateX(+Math.PI/64);
 		}
 		// Look down.
 		if (KeyboardJS.combo.active("down")) {
-			camera.rotateX(-PI/64);
+			camera.rotateX(-Math.PI/64);
 		}
 		// Turn left.
 		if (KeyboardJS.combo.active("left")) {
-			utils.rotateAroundAxis(camera, axes.y, +PI/64);
+			utils.rotateAroundAxis(camera, axes.y, +Math.PI/64);
 		}
 		// Turn right.
 		if (KeyboardJS.combo.active("right")) {
-			utils.rotateAroundAxis(camera, axes.y, -PI/64);
+			utils.rotateAroundAxis(camera, axes.y, -Math.PI/64);
 		}
 		// Move faster.
 		if (KeyboardJS.combo.active("shift")) {
@@ -384,6 +400,7 @@
 	};
 
 	var update = function(clock) {
+		// Make the face creepily horizontally oscillate.
 		if ((~~clock + 5e3) % 1e4 >= 5e3) {
 			models.face.position.y += 0.02;
 		} else {
@@ -403,7 +420,7 @@
 		controls(clock);
 		// Update anything else necessary.
 		update(clock);
-		// Render the scene, from the perspective of the camera.
+		// Render the scene, from the perspective of the main camera.
 		renderer.render(scene, camera);
 	})();
 })(this);
